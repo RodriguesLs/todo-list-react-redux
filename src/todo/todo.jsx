8220ff6e724078d887ec2134fa3479159a350399
Rodrigues <felipe.rodrigues@devires.com.br>
2020-04-16 10:33:06 -0300

    learn(component): include form and list component to organize app in todo folder

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import PageHeader from '../templates/page-header';
+import TodoForm from './todo-form';
+import TodoList from './todo-list';
 
 export default class Todo extends Component {
   render() {
     return (
       <div>
         <PageHeader name='To do' small='register' />
+        <TodoForm />
+        <TodoList />
       </div>
     )
   }
