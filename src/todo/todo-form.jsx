314a4eb9b16b756203d42eded856839d6b6a345c
Rodrigues <felipe.rodrigues@devires.com.br>
2020-04-18 19:26:37 -0300

    feat(many things): ...

--- src/todo/todo-form.jsx
+++ src/todo/todo-form.jsx
@@ -5,10 +5,12 @@ import IconButton from '../templates/icon-button'
 export default props => (
   <div role='form' className='todoForm'>
     <Grid cols='12 9 10'>
-      <input placeholder='Add a task' id="description" className="form-control"/>
+      <input placeholder='Add a task' id="description" className="form-control"
+        onChange={props.handleChange} value={props.description} />
     </Grid>
     <Grid cols='12 3 2'>
       <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton> 
+      <IconButton style='info' icon='search' onClick={props.handleSearch}></IconButton> 
     </Grid>
   </div>
 )
