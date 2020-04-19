6c523d67f60d4b771074109c77e5bb007dec435a
Rodrigues <felipe.rodrigues@devires.com.br>
2020-04-19 09:30:52 -0300

    learn(redux): todo-list redux mode

--- src/todo/todo-list.jsx
+++ src/todo/todo-list.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import IconButton from '../templates/icon-button';
+import { connect } from 'react-redux';
 
-export default props => {
+const TodoList = props => {
   
   const renderRows = () => {
 
@@ -33,5 +34,10 @@ export default props => {
       </tbody>
     </table>
   )
-
 }
+
+const mapStateToProps = state => ({ list: state.todo.list })
+
+export default connect(mapStateToProps)(TodoList)
+
+
